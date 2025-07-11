6084da4b3e9dc61f67a97630ad51f02acbd71755
anveshreddy2006 <padidamanveshreddy.com>
2025-07-11 13:47:10 +0530

    .

--- 12appwrite/src/components/index.js
+++ 12appwrite/src/components/index.js
@@ -6,10 +6,12 @@ import LogoutBtn  from "./Header/LogoutBtn";
 import RTE from "./RTE";
 import Signup from "./Signup";
 import Login from "./Login";
-import Post from "./Post";
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+import Select from "./Select";
+import Button from "./Button";
 export{
     Header,
     Footer,
@@ -19,10 +21,12 @@ export{
     RTE,
     Signup,
     Login,
-    Post,
     PostForm,
      PostCard,
      AuthLayout,
+     Input,
+     Select,
+     Button,
      
 
 
