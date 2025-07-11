2a448411a9703b42309344385cd517c95bbc40ab
anveshreddy2006 <padidamanveshreddy.com>
2025-07-11 13:01:07 +0530

    .

--- 12appwrite/src/components/index.js
+++ 12appwrite/src/components/index.js
@@ -4,6 +4,11 @@ import Container from "./container/Container";
 import Logo  from "./Logo";
 import LogoutBtn  from "./Header/LogoutBtn";
 import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Post from "./Post";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
 export{
     Header,
     Footer,
@@ -11,4 +16,12 @@ export{
     Logo,
     LogoutBtn,
     RTE,
+    Signup,
+    Login,
+    Post,
+    PostForm,
+     PostCard,
+     
+
+
 }
